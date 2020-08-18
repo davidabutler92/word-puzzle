43499bce9089c21fe336e996da6973b552e04f8d
davidabutler92 <davidabutler92@yahoo.com>
2020-08-18 13:48:14 -0700

    add array of vowels have for loop go through splitSentence and have a for loop go through vowel and give conditions

--- js/main.js
+++ js/main.js
@@ -5,5 +5,14 @@ $(document).ready(function() {
     const inputSentence = $("input#sentence").val();
     const splitSentence = inputSentence.split("");
     // console.log(splitSentence);
+    const vowel = ["a", "e", "i", "o", "u"];
+    let newArray = [];
+
+    for(let i = 0; i < splitSentence.length; i++) {
+      for(let j = 0; j < vowel.length; i++) {
+        if(splitSentence);
+      }
+    }
+
   });
 });
