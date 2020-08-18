287be30e3e0eb9583cd1a537aae979cbf2f88cc3
davidabutler92 <davidabutler92@yahoo.com>
2020-08-18 14:34:24 -0700

    add store splitSentence.join in a variable called sentence, hide puzzle-form, show(sentence and text to results

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ $(document).ready(function() {
   $("#puzzle-form").submit(function(event) {
     event.preventDefault();
     const userInput = $("input#sentence").val();
-    // console.log(userInput)
+    const splitSentence = userInput.split("");
+    // console.log(splitSentence);
+    const vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+    for(let i = 0; i <= splitSentence.length; i++) {
+      if(vowel.includes(splitSentence[i])) {
+        splitSentence[i] = ("-");
+      }
+    }
+    newSentence = splitSentence.join("");
+    console.log(newSentence);
+    $("#outputSentence").text(newSentence);
+    $("#puzzle-form").hide();
+    $("#results").show();
+
   });
 });
-
-// const inputSentence = $("input#sentence").val();
-//     const splitArray = inputSentence.split("");
-//     const vowel = ["a", "e", "i", "o", "u"];
-//     let newArray = [];
-
-//     for(let i = 0; i < splitArray.length; i++) {
-//       for(let j = 0; j < vowel.length; j++) {
-//         if(splitArray[i] === vowel[j]) {
-//           newArray.push("-");
-//         }
-//       }
-//     }
