45f8ad1f5ca199d9c3ab4c477fabfbcb3f1f6a15
davidabutler92 <davidabutler92@yahoo.com>
2020-08-18 14:16:08 -0700

    add store userInput in variable, consoloe.log(userInput)'

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   $("#puzzle-form").submit(function(event) {
     event.preventDefault();
-
-    const inputSentence = $("input#sentence").val();
-    const splitSentence = inputSentence.split("");
-    // console.log(splitSentence);
-    const vowel = ["a", "e", "i", "o", "u"];
-    let newArray = [];
-
-    for(let i = 0; i < splitSentence.length; i++) {
-      for(let j = 0; j < vowel.length; i++) {
-        if(splitSentence);
-      }
-    }
-
+    const userInput = $("input#sentence").val();
+    // console.log(userInput)
   });
 });
+
+// const inputSentence = $("input#sentence").val();
+//     const splitArray = inputSentence.split("");
+//     const vowel = ["a", "e", "i", "o", "u"];
+//     let newArray = [];
+
+//     for(let i = 0; i < splitArray.length; i++) {
+//       for(let j = 0; j < vowel.length; j++) {
+//         if(splitArray[i] === vowel[j]) {
+//           newArray.push("-");
+//         }
+//       }
+//     }
